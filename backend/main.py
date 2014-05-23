9890291f98300238ffd05ca06ec3188ca4c848a8
JT Olds <hello@jtolds.com>
2014-05-23 14:51:57 -0600

    teams checkpoint
    quick and dirty - implement all teams backend functionality, with
    minimal frontend.
    
    i spent a while looking into angular, but when i realized we'd have
    to escaped_fragment, pre-rendered html for SEO, i decided server-side
    rendering would keep the codebase simpler for newcomers.
    
    so, basically i bailed cause i'm a coward
    
    anyway, this doesn't talk to the pledge app yet and a bunch of stuff
    is missing, but this supports:
     * login/logout
     * dashboard for logged in users
     * ability to make a new team page
     * ability to edit an existing team page
     * team page urls are based on (editable) team page titles and updated
       when they change, and old urls continue to work
    
    missing:
     * integration with pledge service
     * uploadable images
     * any kind of looking nice
     * markdown support for description field
     * video types other than youtube
    
    misc todo:
     * clean up form validation

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,30 @@
 import Cookie
+import functools
 import json
 import logging
+import os
+import re
 import urllib
+
+import jinja2
 import webapp2
 
 from google.appengine.api import urlfetch
+from google.appengine.ext import db
 
 AUTH_SERVICE_PUB = "https://auth.mayone.us"
-AUTH_SERVICE_REQ = AUTH_SERVICE_PUB  # "http://172.17.0.2"
+AUTH_SERVICE_REQ = "http://172.17.0.2"
+
+JINJA = jinja2.Environment(
+  loader=jinja2.FileSystemLoader('templates/'),
+  extensions=['jinja2.ext.autoescape'],
+  autoescape=True)
+
+YOUTUBE_ID_VALIDATOR = re.compile(r'^[\w\-]+$')
+INTEGER_VALIDATOR = re.compile(r'^[0-9]+$')
+INVALID_SLUG_CHARS = re.compile(r'[^\w-]')
+MULTIDASH_RE = re.compile(r'-+')
+SLUG_TOKEN_AMOUNT = 2
 
 
 class BaseHandler(webapp2.RequestHandler):
@@ -45,17 +62,216 @@ class BaseHandler(webapp2.RequestHandler):
     return "%s/v1/logout?%s" % (AUTH_SERVICE_PUB, urllib.urlencode({
         "return_to": self.request.url}))
 
+  def render_template(self, template, **kwargs):
+    if self.logged_in:
+      data = {
+        "logged_in": True,
+        "current_user": self.current_user,
+        "logout_link": self.logout_link}
+    else:
+      data = {
+        "logged_out": False,
+        "login_links": self.login_links}
+    data.update(kwargs)
+    self.response.write(JINJA.get_template(template).render(data))
+
+  def notfound(self):
+    self.response.status = 404
+    self.render_template("404.html")
+
+
+class Team(db.Model):
+  primary_slug = db.StringProperty()
+  title = db.StringProperty(required=True)
+  description = db.TextProperty(required=True)
+
+  goal_dollars = db.IntegerProperty()
+  website = db.LinkProperty()
+  youtube_id = db.StringProperty()
+  zip_code = db.StringProperty()
+
+  # for use with google.appengine.api.imagesget_serving_url
+  image = db.BlobProperty()
+
+
+class Slug(db.Model):
+  # the key is the slug name
+  team = db.ReferenceProperty(Team, required=True)
+
+  @staticmethod
+  @db.transactional
+  def _make(full_slug, team):
+    e = Slug.get_by_key_name(full_slug)
+    if e is not None:
+      return False
+    Slug(key_name=full_slug, team=team).put()
+    return True
+
+  @staticmethod
+  def new(team):
+    slug_name = MULTIDASH_RE.sub('-', INVALID_SLUG_CHARS.sub('-', team.title))
+    token_amount = SLUG_TOKEN_AMOUNT
+    while True:
+      slug_prefix = os.urandom(token_amount).encode('hex')
+      token_amount += 1
+      full_slug = "%s-%s" % (slug_prefix, slug_name)
+      if Slug._make(full_slug, team):
+        return full_slug
+
+
+class AdminToTeam(db.Model):
+  """This class represents an admin to team relationship, since it's
+  many-to-many
+  """
+  user = db.StringProperty(required=True)  # from current_user["user_id"]
+  team = db.ReferenceProperty(Team, required=True)
+
+
+def require_login(fn):
+  @functools.wraps(fn)
+  def new_handler(self, *args, **kwargs):
+    if not self.logged_in:
+      self.redirect("/")
+      return
+    return fn(self, *args, **kwargs)
+  return new_handler
+
 
 class IndexHandler(BaseHandler):
   def get(self):
+    self.render_template("index.html")
+
+
+class NotFoundHandler(BaseHandler):
+  def get(self):
+    self.notfound()
+
+
+class TeamBaseHandler(BaseHandler):
+  def validate(self, slug):
+    s = Slug.get_by_key_name(slug)
+    if s is None:
+      self.notfound()
+      return None, False, False
+    team = s.team
+    if team is None:
+      self.notfound()
+      return None, False, False
+    primary = True
+    if team.primary_slug and team.primary_slug != slug:
+      primary = False
+    is_admin = False
     if self.logged_in:
-      self.response.write('<a href="%s">logout</a><br/>' % self.logout_link)
-      self.response.write(str(self.current_user))
+      if AdminToTeam.all().filter("team =", team).filter(
+          "user =", str(self.current_user["user_id"])).get() is not None:
+        is_admin = True
+    return team, primary, is_admin
+
+
+class TeamHandler(TeamBaseHandler):
+  def get(self, slug):
+    team, primary, is_admin = self.validate(slug)
+    if team is None:
+      return
+    if not primary:
+      return self.redirect("/t/%s" % team.primary_slug, permanent=True)
+    if is_admin:
+      edit_url = "/t/%s/edit" % team.primary_slug
     else:
-      for service in self.login_links:
-        self.response.write('<a href="%s">log in via %s</a><br/>' %
-                            (self.login_links[service], service))
+      edit_url = None
+    self.render_template("show_team.html", team=team, edit_url=edit_url)
+
+
+class DashboardHandler(BaseHandler):
+  @require_login
+  def get(self):
+    teams = [a.team for a in
+             AdminToTeam.all().filter('user =',
+                str(self.current_user["user_id"]))]
+    self.render_template("dashboard.html", teams=teams)
+
+
+class NewTeamHandler(BaseHandler):
+  @require_login
+  def get(self):
+    self.render_template("new_team.html")
+
+  @require_login
+  def post(self):
+    # TODO: this is horrible. should use WTForms or something and
+    # NOT COPY PASTE
+    title = self.request.get("title")
+    description = self.request.get("description")
+    goal_dollars = self.request.get("goal_dollars") or None
+    if goal_dollars:
+      goal_dollars = int(goal_dollars)
+    website = self.request.get("website") or None
+    youtube_id = self.request.get("youtube_id") or None
+    if youtube_id and not YOUTUBE_ID_VALIDATOR.match(youtube_id):
+      raise Exception("invalid youtube id")
+    zip_code = self.request.get("zip_code") or None
+    if zip_code and not INTEGER_VALIDATOR.match(zip_code):
+      raise Exception("invalid zip_code")
+    team = Team(title=title, description=description,
+                goal_dollars=goal_dollars * 100, website=website,
+                youtube_id=youtube_id, zip_code=zip_code)
+    team.put()
+    # TODO: can i reference a team before putting it in other reference
+    # properties? should check
+    AdminToTeam(user=str(self.current_user["user_id"]), team=team).put()
+    team.primary_slug = Slug.new(team)
+    team.put()
+    self.redirect("/t/%s" % team.primary_slug)
+
+
+class EditTeamHandler(TeamBaseHandler):
+
+  @require_login
+  def get(self, slug):
+    team, primary, is_admin = self.validate(slug)
+    if team is None:
+      return
+    if not primary:
+      return self.redirect("/t/%s/edit" % team.primary_slug, permanent=True)
+    if not is_admin:
+      return self.redirect("/t/%s" % team.primary_slug)
+    self.render_template("edit_team.html", team=team)
+
+  @require_login
+  def post(self, slug):
+    team, _, is_admin = self.validate(slug)
+    if team is None:
+      return
+    if not is_admin:
+      return self.redirect("/t/%s" % team.primary_slug)
+
+    # TODO: this is horrible. should use WTForms or something and
+    # NOT COPY PASTE
+    team.title = self.request.get("title")
+    team.description = self.request.get("description")
+    goal_dollars = self.request.get("goal_dollars") or None
+    if goal_dollars:
+      team.goal_dollars = int(goal_dollars)
+    else:
+      team.goal_dollars = None
+    team.website = self.request.get("website") or None
+    youtube_id = self.request.get("youtube_id") or None
+    if youtube_id and not YOUTUBE_ID_VALIDATOR.match(youtube_id):
+      raise Exception("invalid youtube id")
+    team.youtube_id = youtube_id
+    zip_code = self.request.get("zip_code") or None
+    if zip_code and not INTEGER_VALIDATOR.match(zip_code):
+      raise Exception("invalid zip_code")
+    team.zip_code = zip_code
+    team.primary_slug = Slug.new(team)
+    team.put()
+    self.redirect("/t/%s" % team.primary_slug)
 
 
 app = webapp2.WSGIApplication([
-  (r'/.*', IndexHandler)], debug=False)
+  (r'/t/([^/]+)/?', TeamHandler),
+  (r'/t/([^/]+)/edit?', EditTeamHandler),
+  (r'/dashboard/?', DashboardHandler),
+  (r'/dashboard/new/?', NewTeamHandler),
+  (r'/?', IndexHandler),
+  (r'.*', NotFoundHandler)], debug=False)
