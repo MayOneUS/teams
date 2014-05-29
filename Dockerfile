0e53e0a6c1ddc3d55dc77be746f95fb0fa9d98f4
JT Olds <hello@jtolds.com>
2014-05-28 18:51:58 -0600

    fix port forwarding

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,5 @@
 FROM jtolds/mayone-gae
 
 ADD . /develop
+
+EXPOSE 443 8000
--- start-with-docker.sh
+++ start-with-docker.sh
@@ -2,4 +2,4 @@
 
 docker build -t mayone-teams-dev .
 
-docker run -t -i mayone-teams-dev /develop/.docker-entry.sh
+docker run -t -i -p 127.0.0.1:443:443 mayone-teams-dev /develop/.docker-entry.sh
