960277d0fefee9d171a232cace8ae8f0cc7afb4a
JT Olds <hello@jtolds.com>
2014-06-02 01:54:35 -0600

    redirect to login

--- main.py
+++ main.py
@@ -285,7 +285,7 @@ class IndexHandler(BaseHandler):
   def get(self):
     if self.logged_in:
       return self.redirect("/dashboard")
-    return self.redirect("https://mayday.us")
+    return self.redirect("/login")
 
 
 class NotFoundHandler(BaseHandler):
